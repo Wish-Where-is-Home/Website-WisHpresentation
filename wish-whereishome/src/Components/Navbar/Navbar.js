a3bb2cf1d31f7646dfccd432759e29b148019f7c
Vasco_Faria <vascomfaria@ua.pt>
2024-02-23 02:23:37 +0000

    error hrefs

--- wish-whereishome/src/Components/Navbar/Navbar.js
+++ wish-whereishome/src/Components/Navbar/Navbar.js
@@ -11,25 +11,25 @@ const Navbar = ({ isOpen, toggleNavbar }) => {
         <div className='navbar-div'>
             <div className='navbar-title'>
                 <h3>Where is Home</h3>
-                <a href="#" onClick={toggleNavbar}>
+                <a href="#0" onClick={toggleNavbar}>
                     <FontAwesomeIcon className='close' icon={faTimes} />
                 </a>
             </div>
             <div className='navbar-content'>
                 <ul className='navbar-list'>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Product</a></li>
-                    <li><a href="#">Presentations</a></li>
-                    <li><a href="#">Documents</a></li>
-                    <li><a href="#">Architecture</a></li>
-                    <li><a href="#">Calendar</a></li>
-                    <li><a href="#">Team</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href="#0">Home</a></li>
+                    <li><a href="#0">Product</a></li>
+                    <li><a href="#0">Presentations</a></li>
+                    <li><a href="#0">Documents</a></li>
+                    <li><a href="#0">Architecture</a></li>
+                    <li><a href="#0">Calendar</a></li>
+                    <li><a href="#0">Team</a></li>
+                    <li><a href="#0">Contact</a></li>
                 </ul>
             </div>
             <div className='navbar-github'>
                 <p>Follow Us</p>
-                <a href="https://github.com/Wish-Where-is-Home" className="github-icon" target="_blank">
+                <a href="https://github.com/Wish-Where-is-Home" className="github-icon" target="_blank" rel="noreferrer">
                     <FontAwesomeIcon icon={faGithub} />
                 </a>
             </div>  
