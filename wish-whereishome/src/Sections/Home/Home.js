89790632e18701a797a70fbc50517b1f4c72576e
Vasco_Faria <vascomfaria@ua.pt>
2024-02-23 22:53:00 +0000

    bug fixed netlify

--- wish-whereishome/src/Sections/Home/Home.js
+++ wish-whereishome/src/Sections/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
@@ -6,7 +6,7 @@ import ScrollDownButton from '../../Components/ScrollDownButton/ScrollDownButton
 import { Link } from 'react-scroll';
 
 function Home() {
-  const [isLoaded, setIsLoaded] = useState(true);
+  const [isLoaded] = useState(true);
 
 
   return (
