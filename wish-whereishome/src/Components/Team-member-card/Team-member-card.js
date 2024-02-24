fbcf390e10d33351ad0bc82b8cbd988f71b15097
Vasco_Faria <vascomfaria@ua.pt>
2024-02-24 15:20:03 +0000

    Team & Mentors Section Done

---
+++ wish-whereishome/src/Components/Team-member-card/Team-member-card.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Team-member-card.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import user from '../../Assets/default-user.png';
+
+const TeamMemberCard = ({name,email, role, github}) =>{
+ 
+    return (
+        <div className='team-member-card'>
+            <img src={user} alt='default-user'></img>
+            <h3>{name}</h3>
+            <span>{email}</span>
+            <span>{role}</span>
+            <div className='team-social'>
+                <a href={github}  target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} className='github-team' color="#2f95ac"/>
+                </a>
+            </div>
+      </div>
+    );
+  }
+
+
+export default TeamMemberCard;
--- wish-whereishome/src/Sections/Team/Team.js
+++ wish-whereishome/src/Sections/Team/Team.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './Team.css';
 
+import TeamMemberCard from '../../Components/Team-member-card/Team-member-card';
+
 const Team = () => {
  
 
     return (
       <div id="team" className='team-section'>
-        <></>
+        <div className='team-content'>
+            <div className='row-team-h3'>
+              <h3>/ Team & Mentors</h3>
+            </div>
+            <div className='row-team-p'>
+                <p>Our dedicated team of students and mentors from  <a href='https://www.ua.pt/' target="_blank" rel="noreferrer" >Universidade de Aveiro</a></p>
+            </div>
+            <div className='show-team-cards'>
+              <TeamMemberCard name="Cristiano Nicolau" email="cristianonicolau@ua.pt" role="Student" github="https://github.com/cristiano-nicolau" />
+              <TeamMemberCard name="Gonçalo Lopes"  email="goncalorcml@ua.pt" role="Student" github="https://github.com/joanaagomesua"/>
+              <TeamMemberCard name="Miguel Miragaia"  email="miguelmiragaia@ua.pt" role="Student" github="https://github.com/Miragaia"/>
+              <TeamMemberCard name="Joana Gomes"  email="joanaagomes@ua.pt" role="Student" github="https://github.com/MoreiraLopes"/>
+              <TeamMemberCard name="Tiago Cruz"  email="tiagofcruz78@ua.pt" role="Student" github="https://github.com/TiagoC18"/>
+              <TeamMemberCard name="Vasco Faria"  email="vascomfaria@ua.pt" role="Student" github="https://github.com/Vasco-Faria"/>
+              <TeamMemberCard name="José Moreira"  email=" " role="Mentor" github="https://github.com/josemoreiraUA"/>
+            </div>
+        </div>
       </div>
     );
   }
