79672b2c5f746b6041ec44beedc8e66b6b77e05d
Vasco_Faria <vascomfaria@ua.pt>
2024-02-26 00:20:02 +0000

    footer done

--- wish-whereishome/src/Sections/Footer/Footer.js
+++ wish-whereishome/src/Sections/Footer/Footer.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import './Footer.css';
+import logo from '../../Assets/logo.jpg';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
     <div className='footer-section' >
       <div className='footer-container'>
-        <div>
+        <div className='footer-github'>
+          <a href="https://github.com/Wish-Where-is-Home" className='footer-icon' target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
         </div>
-        <div>
-
+        <div  className="footer-content">
+          <p className='footer-title'>Projeto em Informatica (3º ano LEI)</p>
+          <a href='https://www.ua.pt/' target="_blank" rel="noreferrer" >Universidade de Aveiro</a>
         </div>
-        <div>
-
+        <div className='footer-copyright'>
+          <img src={logo}></img>
+          <p>Copyright  © 2024  Where is Home</p>
         </div>
       </div>
     </div>
