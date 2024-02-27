db07c706636ce1f879a7d2deb7290eb4aa94ab03
Vasco_Faria <vascomfaria@ua.pt>
2024-02-27 21:43:37 +0000

    Presentation fixed

--- wish-whereishome/src/Components/DownloadPresentations/DownloadPresentations.js
+++ wish-whereishome/src/Components/DownloadPresentations/DownloadPresentations.js
@@ -1,34 +1,32 @@
 import React from 'react';
 import './DownloadPresentations.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
-
-
-const DownloadPresentations = ({file,filename, text,img}) => {
-
+const DownloadPresentations = ({ file, filename, text, img }) => {
   const handleDownload = () => {
-   
-    const link = document.createElement('a');
-    link.href = {file};
-    link.download = {filename};
-    link.click();
+    if (file) {
+      const link = document.createElement('a');
+      link.href = file;
+      link.download = filename;
+      link.click();
+    }
   };
 
   return (
-    <div className="powerpoint-carddd" >
-    <div className="powerpoint-card">
-      <img src={img} alt="No Powerpoint Available" />
-      <div className="overlay" onClick={handleDownload}>
-      <FontAwesomeIcon className='transfer-icon' icon={faDownload} />
+    <div className="powerpoint-carddd">
+      <div className="powerpoint-card">
+        <img src={img} alt="No Powerpoint Available" />
+        {file && (
+          <div className="overlay" onClick={handleDownload}>
+            <FontAwesomeIcon className='transfer-icon' icon={faDownload} />
+          </div>
+        )}
       </div>
-
+      <div className='powerpoint-text'>
+        <p>{text}</p>
+      </div>
     </div>
-    <div className='powerpoint-text'>
-    <p>{text}</p>
-  </div>
-</div>
   );
 };
 
