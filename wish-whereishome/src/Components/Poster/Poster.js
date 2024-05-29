8a3fa81cd043fd1536506d255a6feb34430d9cdd
Vasco_Faria <vascomfaria@ua.pt>
2024-05-29 22:21:02 +0100

    update

--- wish-whereishome/src/Components/Poster/Poster.js
+++ wish-whereishome/src/Components/Poster/Poster.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Poster.css';
 import DownloadPresentations from '../../Components/DownloadPresentations/DownloadPresentations';
 import posterimage from '../../Assets/poster.png';
-import poster1 from '../../Assets/poster.jpg';
+import poster1 from '../../Assets/ST2024-Students-Exemplo-Poster.png';
 
 
 
@@ -14,14 +14,19 @@ const Poster = () => {
 
             <div className='poster-content'>
               <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '40vh' }}>
-                <DownloadPresentations filename="ST2024-Students-Exemplo-Poster.pdf" file="/Pptx/ST2024-Students-Exemplo-Poster.pdf" text="Download Poster"   img={posterimage}/>
+                <DownloadPresentations filename="ST2024-Students-Exemplo-Poster.pdf" file="/Pptx/ST2024-Students-Exemplo-Poster.pdf" text="Download Poster"   img={poster1}/>
               </div>
                 <div className='poster-img'>
                     <img src={poster1}>
                     </img>
                 </div>
             </div>
+            <div id="modal" class="modal">
+        <span class="close">&times;</span>
+        <img class="modal-content" id="imgModal"/>
     </div>
+    </div>
+    
   );
 };
 
